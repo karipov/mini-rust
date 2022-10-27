23b086ce273efaea0ec44752134d12f487a84096
karipov <karipov@protonmail.com>
2022-10-27 14:12:34 -0400

    p2 design find contains

--- week2/starter/src/p2_design.rs
+++ week2/starter/src/p2_design.rs
@@ -12,25 +12,25 @@
 ///   * v: representing a collection of numbers
 /// and rounds every number in-place in v to the nearest integer.
 pub fn round_all(
-  // (1) v could be a Vec<_>, &Vec<_>, &mut Vec<_>, &[_], or &mut[_]. I choose &mut[_] because
-  //     we do not need to change the size or order of the collection, but do need to change the elements.
-  // (2) v could be a &mut [{number type}], and "round to the nearest integer" implies the use of floats.
-  // (3) The choice of f32 vs. f64 is arbitrary -- we would need to use more advanced features to write one
-  //     function that works for both types, so we arbitrarily pick f32 for now.
-  v: &mut [f32],
+    // (1) v could be a Vec<_>, &Vec<_>, &mut Vec<_>, &[_], or &mut[_]. I choose &mut[_] because
+    //     we do not need to change the size or order of the collection, but do need to change the elements.
+    // (2) v could be a &mut [{number type}], and "round to the nearest integer" implies the use of floats.
+    // (3) The choice of f32 vs. f64 is arbitrary -- we would need to use more advanced features to write one
+    //     function that works for both types, so we arbitrarily pick f32 for now.
+    v: &mut [f32],
 )
 // No return value, since this function only mutates an input.
 {
-  for n in v.iter_mut() {
-    *n = n.round();
-  }
+    for n in v.iter_mut() {
+        *n = n.round();
+    }
 }
 
 #[test]
 fn round_all_test() {
-  let mut v = vec![0.3, 0.7];
-  round_all(&mut v);
-  assert_eq!(v, vec![0., 1.]);
+    let mut v = vec![0.3, 0.7];
+    round_all(&mut v);
+    assert_eq!(v, vec![0., 1.]);
 }
 
 // Now you try!
@@ -40,16 +40,34 @@ fn round_all_test() {
 ///   * needle: representing a particular string
 /// and returns a value:
 ///   * representing which strings in the collection contain the needle
-pub fn find_contains(
-  /* Pick and justify your parameters */
-) -> () /* Pick and justify your return type */
+pub fn find_contains<'a>(
+    // (1) haystack could be a Vec<String>, &Vec<String>, &Vec<&str>, &[&str]. I choose &[&str]
+    //     because I do not need ownership of the string and we do not need to change the size of the
+    //     input collection, therefore a slice.
+    // (2) needle could be String, &str. I choose &str due to above reasons!
+    // also i'm getting a bunch of lifetime problems... i did what the rust compiler told me
+    haystack: &'a [&str],
+    needle: &str,
+) -> Vec<&'a str> /* We return pointers to the string literals containing the needle. I use a Vec
+                    type here because arrays are created on the stack and we also don't know how
+                    many such valid strings we may get */
 {
-  unimplemented!();
+    let mut hays: Vec<&str> = Vec::new();
+    for &hay in haystack.iter() {
+        if hay.contains(needle) {
+            hays.push(hay);
+        }
+    }
+
+    hays
 }
 
 #[test]
 fn find_contains_test() {
-  /* Add your unit test here! */
+    let v = ["abc", "de", "ab", "a", "f"];
+    let out = find_contains(&v, "a");
+
+    assert_eq!(out, vec!["abc", "ab", "a"]);
 }
 
 /// P2b: fill_progress_bar is a function that takes:
@@ -60,13 +78,23 @@ fn find_contains_test() {
 /// For example, at a progress of 20% with bracketed delimiters, the bar would be:
 ///   [==        ]
 pub fn fill_progress_bar(
-  /* Pick and justify your parameters */
-) -> () /* Pick and justify your return type */
+    // (1) buf could be a Vec<String>, &mut Vec<String> [question: does this mutably borrow, ref, or consume String?]
+    //     String, &mut String, &mut str. I choose &mut str because I do not need to change the length
+    //     of the string buffer I receive. Knowing the size of the str is enough!
+    // (2) delims could be a String, &String, &str, char. I choose char because that seems simpler
+    //     than checking if str has only one character, using that, etc.
+    // (3) frac can be any f-type, i-type, u-type. I chose f32 for simplicity of programming,
+    //     so percentages have to be represented as floats between 0 and 1
+    buf: &mut &str,
+    delims: char,
+    frac: f32,
+)
+// No return value, since this function only mutates an input.
 {
-  unimplemented!();
+    let chars_to_fill: u32 = (((buf.len() - 2) as f32) * frac) as u32;
 }
 
 #[test]
 fn test_fill_progress_bar() {
-  /* Add your unit test here! */
+    /* Add your unit test here! */
 }
